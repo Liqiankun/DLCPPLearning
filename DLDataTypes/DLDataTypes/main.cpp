553cc5a099505ad128b1f83bfb97145f36dd014e
David <qiankunli@yahoo.com>
2019-01-14 22:38:20 +0800

    data type

---
+++ DLDataTypes/DLDataTypes/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  DLDataTypes
+//
+//  Created by David on 2019/1/14.
+//  Copyright © 2019 David. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    char grad = 'A';
+    string name = "David Lee";
+    int age = 26;
+    float height = 3.4;
+    double gpa = 2.35;
+    bool isMale = true;
+    
+    return 0;
+}
